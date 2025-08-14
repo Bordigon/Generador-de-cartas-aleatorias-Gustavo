29bdb38379cceacd5cec49ca89a3ec90af76571e
Bordigon <gistavovg@gmail.com>
2025-08-13 22:39:48 -0400

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+const valor = ["A","1","2","3","4","5","6","7","8","9","10","J","Q","k"];
+const figura = ["corazon","trebol","diamante","pica"];
 
+function indice(numero){
+  return `<p>${valor[numero]}</p>`;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function imagen(valor){
+  return `<img src="pictures/${figura[valor]}.jpg">`;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = getRandomInt(13);
+  let figura = getRandomInt(4);
+ document.getElementById("indice").innerHTML=indice(numero) + imagen(figura);
+ document.querySelector(".invertido").innerHTML=indice(numero) + imagen(figura);
 };
